ccb405f98703d365cf9908d3261c0a5e0321287e
tonojuarezgt <tonojuarezgt@gmail.com>
2014-02-13 22:03:41 -0600

    commit inicial

---
+++ src/com/example/demo2/CountryDetailActivity.java
@@ -0,0 +1,36 @@
+package com.example.demo2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class CountryDetailActivity extends FragmentActivity {
+    private String country = "";
+	public static final String COUNTRY = "country";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_country_detail);
+	
+		Intent intent = getIntent();
+		country = intent.getStringExtra(COUNTRY);
+		
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.country_detail, menu);
+		return true;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+
+}
---
+++ src/com/example/demo2/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.demo2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends FragmentActivity implements OnItemClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		String[] array_countries = new String[]{"Brasil", "México", "Colombia", "Argentina",
+				"Perú", "Venezuela", "Chile", "Ecuador", 
+				"Guatemala", "Cuba"
+				};
+		ArrayList<String> countries = new ArrayList<String>(Arrays.asList(array_countries));
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
+			    											android.R.layout.simple_list_item_1, 
+			    											countries);
+
+		ListView list = (ListView)findViewById(R.id.listView);
+		list.setAdapter(adapter);
+		list.setOnItemClickListener(this);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> adapterView, View view, int position, long arg3) {
+		String country = adapterView.getItemAtPosition(position).toString();
+		if (getResources().getConfiguration().orientation == 
+				Configuration.ORIENTATION_LANDSCAPE) {
+			FragmentManager manager = getSupportFragmentManager(); 
+			CountryInfoFragment fragment = (CountryInfoFragment) manager.findFragmentById(R.id.fragmentCountryInfo);
+			fragment.loadWebViewContent(country);
+		} else {
+			Intent intent = new Intent(this, CountryDetailActivity.class);
+			intent.putExtra(CountryDetailActivity.COUNTRY, country);
+			startActivity(intent);			
+		}
+
+	}
+
+}
